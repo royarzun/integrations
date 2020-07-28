5519ae4ec8a5986b58276b35bb69ff942a6047d5
Ricardo Oyarzun <royarzun@gmail.com>
2020-07-28 19:26:31 -0400

    chore: bump up version

--- integration/app.py
+++ integration/app.py
@@ -1,29 +1,35 @@
-from flask import jsonify, request
+from flask import jsonify, request, Flask
 
-from integration.rest_service.settings import run
-
-app, membership_service = run()
-
-
-@app.route("/authenticate", methods=["POST"])
-def authenticate():
-    try:
-        membership_data = membership_service.get_membership_identifier(
-            jsonify(request.data)
-        )
-        if membership_data is None:
-            return {}, 403
-
-        return jsonify(membership_data)
-    except:
-        return {}, 503
+from integration.rest_service.api_client import BaseAPIClient
+from integration.rest_service.service import MembershipService
 
 
-@app.route("/validate", methods=["POST"])
-def validate():
-    try:
-        return jsonify(
-            {"is_active": membership_service.is_active(jsonify(request.data))}
-        )
-    except:
-        return {}, 503
+def run_app(cls):
+    assert issubclass(
+        cls, BaseAPIClient
+    ), "API Client class requires to extend from BaseAPIClient class"
+    membership_service = MembershipService(api_client=cls())
+    app = Flask(__name__)
+
+    def authenticate():
+        try:
+            membership_data = membership_service.get_membership_identifier(request.data)
+            if membership_data is None:
+                return {}, 403
+
+            return membership_data
+        except:
+            return {}, 503
+
+    def validate():
+        try:
+            return jsonify(
+                {"is_active": membership_service.is_active(request.data)}
+            )
+        except:
+            return {}, 503
+
+    app.add_url_rule("/authenticate", "/authenticate", view_func=authenticate)
+    app.add_url_rule("/validate", "/validate", view_func=validate)
+    app.route(methods=["post"], rule=None)
+    return app
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name="loyalty_integrations",
-    version="0.2dev",
+    version="0.4dev",
     packages=setuptools.find_packages(),
     license="",
     long_description=open("README.md").read(),
