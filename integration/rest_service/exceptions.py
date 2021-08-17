c7bac8511778942673820089eaa66438a19223f9
Jefferson <jefferson@cornershopapp.com>
2021-08-16 22:23:32 -0400

    feat(exceptions): rename generic exception to handled satellite exception
    UnusableMembership and InvalidMembership inherit from HandledSatelliteException
    
    remove status code and raise default status code

--- integration/rest_service/exceptions.py
+++ integration/rest_service/exceptions.py
@@ -1,28 +1,27 @@
-class GenericSatelliteException(Exception):
-    status_code = 500
-    error_code = ""
-
-
 class ServiceUnavailableException(Exception):
-    status_code = 503
+    pass
 
 
 class ImproperlyConfigured(Exception):
     pass
 
 
-class UnusableMembership(Exception):
-    # Valid membership but deleted, expired, canceled, etc
-    pass
-
-
-class InvalidMembership(Exception):
-    # Not existent membership
-    pass
-
-
-class BadRequest(GenericSatelliteException):
+class HandledSatelliteException(Exception):
     status_code = 400
+    error_code = ""
+
+
+class UnusableMembership(HandledSatelliteException):
+    # Valid membership but deleted, expired, canceled, etc
+    error_code = "UNUSABLE_MEMBERSHIP"
+
+
+class InvalidMembership(HandledSatelliteException):
+    # Not existent membership
+    error_code = "INVALID_MEMBERSHIP"
+
+
+class BadRequest(HandledSatelliteException):
     error_code = "BAD_REQUEST"
 
 
@@ -30,6 +29,6 @@ class SearchFilterLimitReached(BadRequest):
     error_code = "SEARCH_FILTER_LIMIT_REACHED"
 
 
-class NotFound(GenericSatelliteException):
+class NotFound(HandledSatelliteException):
     status_code = 404
     error_code = "NOT_FOUND"
